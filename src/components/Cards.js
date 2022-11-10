b1e5a8bce8fa2aab42dcfa7b39513ac55417a3f3
Felix Lai <felix.lai@hotmail.com>
2022-11-10 15:55:10 -0500

    moved images and updated imports

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,22 +3,22 @@ import Card from './Card'
 
 export default function Cards() {
     const [items, setItems] =useState([
-        { id: 1, img: '/src/assets/images/html.png', stat: ""},
-        { id: 1, img: '/src/assets/images/html.png', stat: ""},
-        { id: 2, img: '/src/assets/images/css.png', stat: ""},
-        { id: 2, img: '/src/assets/images/css.png', stat: ""},
-        { id: 3, img: '/src/assets/images/javascript.png', stat: ""},
-        { id: 3, img: '/src/assets/images/javascript.png', stat: ""},
-        { id: 4, img: '/src/assets/images/sass.png', stat: ""},
-        { id: 4, img: '/src/assets/images/sass.png', stat: ""},
-        { id: 5, img: '/src/assets/images/react.png', stat: ""},
-        { id: 5, img: '/src/assets/images/react.png', stat: ""},
-        { id: 6, img: '/src/assets/images/responsive.jpeg', stat: ""},
-        { id: 6, img: '/src/assets/images/responsive.jpeg', stat: ""},
-        { id: 7, img: '/src/assets/images/rest-api.webp', stat: ""},
-        { id: 7, img: '/src/assets/images/rest-api.webp', stat: ""},
-        { id: 8, img: '/src/assets/images/nodejs.jpeg', stat: ""},
-        { id: 8, img: '/src/assets/images/nodejs.jpeg', stat: ""}
+        { id: 1, img: '/img/html.png', stat: ""},
+        { id: 1, img: '/img/html.png', stat: ""},
+        { id: 2, img: '/img/css.png', stat: ""},
+        { id: 2, img: '/img/css.png', stat: ""},
+        { id: 3, img: '/img/javascript.png', stat: ""},
+        { id: 3, img: '/img/javascript.png', stat: ""},
+        { id: 4, img: '/img/sass.png', stat: ""},
+        { id: 4, img: '/img/sass.png', stat: ""},
+        { id: 5, img: '/img/react.png', stat: ""},
+        { id: 5, img: '/img/react.png', stat: ""},
+        { id: 6, img: '/img/responsive.jpeg', stat: ""},
+        { id: 6, img: '/img/responsive.jpeg', stat: ""},
+        { id: 7, img: '/img/rest-api.webp', stat: ""},
+        { id: 7, img: '/img/rest-api.webp', stat: ""},
+        { id: 8, img: '/img/nodejs.jpeg', stat: ""},
+        { id: 8, img: '/img/nodejs.jpeg', stat: ""}
     ].sort(() => Math.random() - 0.5))
 
     const [prev, setPrev] = useState(-1)

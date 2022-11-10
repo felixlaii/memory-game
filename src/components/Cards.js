2365a2275eafffbe618c6a832d4187483ceaa0b3
Felix Lai <felix.lai@hotmail.com>
2022-11-10 15:27:11 -0500

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,7 +2,24 @@ import React from 'react'
 import Card from './Card'
 
 export default function Cards() {
-    
+    const [items, setItems] =useState([
+        { id: 1, img: '/src/assets/images/html.png', stat: ""},
+        { id: 1, img: '/src/assets/images/html.png', stat: ""},
+        { id: 2, img: '/src/assets/images/html.png', stat: ""},
+        { id: 2, img: '/src/assets/images/html.png', stat: ""},
+        { id: 3, img: '/src/assets/images/html.png', stat: ""},
+        { id: 3, img: '/src/assets/images/html.png', stat: ""},
+        { id: 4, img: '/src/assets/images/html.png', stat: ""},
+        { id: 4, img: '/src/assets/images/html.png', stat: ""},
+        { id: 5, img: '/src/assets/images/html.png', stat: ""},
+        { id: 5, img: '/src/assets/images/html.png', stat: ""},
+        { id: 6, img: '/src/assets/images/html.png', stat: ""},
+        { id: 6, img: '/src/assets/images/html.png', stat: ""},
+        { id: 7, img: '/src/assets/images/html.png', stat: ""},
+        { id: 7, img: '/src/assets/images/html.png', stat: ""},
+        { id: 8, img: '/src/assets/images/html.png', stat: ""},
+        { id: 8, img: '/src/assets/images/html.png', stat: ""}
+    ])
   return (
     <div>Cards</div>
   )

2e9cd7b50dabda2d4897b240cadcb3223693e8e9
Felix Lai <felix.lai@hotmail.com>
2022-11-10 15:29:43 -0500

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,20 +5,20 @@ export default function Cards() {
     const [items, setItems] =useState([
         { id: 1, img: '/src/assets/images/html.png', stat: ""},
         { id: 1, img: '/src/assets/images/html.png', stat: ""},
-        { id: 2, img: '/src/assets/images/html.png', stat: ""},
-        { id: 2, img: '/src/assets/images/html.png', stat: ""},
-        { id: 3, img: '/src/assets/images/html.png', stat: ""},
-        { id: 3, img: '/src/assets/images/html.png', stat: ""},
-        { id: 4, img: '/src/assets/images/html.png', stat: ""},
-        { id: 4, img: '/src/assets/images/html.png', stat: ""},
-        { id: 5, img: '/src/assets/images/html.png', stat: ""},
-        { id: 5, img: '/src/assets/images/html.png', stat: ""},
-        { id: 6, img: '/src/assets/images/html.png', stat: ""},
-        { id: 6, img: '/src/assets/images/html.png', stat: ""},
-        { id: 7, img: '/src/assets/images/html.png', stat: ""},
-        { id: 7, img: '/src/assets/images/html.png', stat: ""},
-        { id: 8, img: '/src/assets/images/html.png', stat: ""},
-        { id: 8, img: '/src/assets/images/html.png', stat: ""}
+        { id: 2, img: '/src/assets/images/css.png', stat: ""},
+        { id: 2, img: '/src/assets/images/css.png', stat: ""},
+        { id: 3, img: '/src/assets/images/javascript.png', stat: ""},
+        { id: 3, img: '/src/assets/images/javascript.png', stat: ""},
+        { id: 4, img: '/src/assets/images/sass.png', stat: ""},
+        { id: 4, img: '/src/assets/images/sass.png', stat: ""},
+        { id: 5, img: '/src/assets/images/react.png', stat: ""},
+        { id: 5, img: '/src/assets/images/react.png', stat: ""},
+        { id: 6, img: '/src/assets/images/responsive.jpeg', stat: ""},
+        { id: 6, img: '/src/assets/images/responsive.jpeg', stat: ""},
+        { id: 7, img: '/src/assets/images/rest-api.webp', stat: ""},
+        { id: 7, img: '/src/assets/images/rest-api.webp', stat: ""},
+        { id: 8, img: '/src/assets/images/nodejs.jpeg', stat: ""},
+        { id: 8, img: '/src/assets/images/nodejs.jpeg', stat: ""}
     ])
   return (
     <div>Cards</div>
